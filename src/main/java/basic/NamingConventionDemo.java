8a6cbf5e87fa5af18cf8ac8c6e1591e392b81a0c
lamtakloong <lamtakloong@gmail.com>
2020-04-28 21:17:51 +0800

    Initial Commit: Basic structure and simple code

---
+++ src/main/java/basic/NamingConventionDemo.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class NamingConventionDemo {
+
+  // All word must upper
+  final double PI = 3.1415926;
+
+  final int MAX_VALUE = Integer.MAX_VALUE;
+
+  public static void main(String[] args) {
+
+
+
+  }
+  public void computCircleArea(){
+    String usernameAndPassword;
+
+  }
+
+}
---
+++ src/main/java/basic/NumberTypeDemo.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class NumberTypeDemo {
+
+  public static void main(String[] args) {
+    int b1 = 127+1;
+    long l1 = 100l;
+    float f1 = 1.0f;
+    double d1 = 1.0;
+  }
+}
