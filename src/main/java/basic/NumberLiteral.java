c96b60815effa8e9f78db1f3381df9768f644726
lamtakloong <lamtakloong@gmail.com>
2020-05-21 13:07:25 +0800

    2020-5-21

---
+++ src/main/java/basic/NumberLiteral.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class NumberLiteral {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(0B1111);//15
+        System.out.println(07777);//4095
+        System.out.println(0xFFFF);//65535
+
+    }
+}
